daa26e5cc4a7010afd9c037c1032623ce01f1c55
deondredavis <67431986+deondredavis@users.noreply.github.com>
2023-03-07 16:00:44 -0500

    Add files via upload

---
+++ Basic FizzBuzz Exercise.py
@@ -0,0 +1,15 @@
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+
+'''
+example: print(fizz_buzz(15))
+
+expected output: FizzBuzz
+'''
